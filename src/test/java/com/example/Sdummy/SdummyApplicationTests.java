c7901801553ba6210373c842b53e0633bc941f85
Anil, Khanal <khanalaneel@gmail.com>
2023-02-13 14:05:05 -0500

    Initial Commit

---
+++ src/test/java/com/example/Sdummy/SdummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sdummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
